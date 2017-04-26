4689c5d062a40a7c9067c58c17ddd71abda28c9b
sena <sena@sena-MS-7817>
2017-04-26 11:25:07 -0500

    usando el trabjo en equipo 02

--- src/javaapplication9/JavaApplication9.java
+++ src/javaapplication9/JavaApplication9.java
@@ -3,21 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication8;
+package javaapplication9;
 
 /**
  *
  * @author sena
  */
-public class JavaApplication8 {
+public class JavaApplication9 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo como estan");
-        System.out.print("hello ");
-        System.out.print("hello a ");
+        // TODO code application logic here
     }
     
 }
